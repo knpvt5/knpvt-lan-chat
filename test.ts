952fcef34e05beeb96ae04e4973102dd08fe122a
knpvt5 <knpvt5@gmail.com>
2026-05-09 10:44:04 +0530

    Clean up test.ts by removing unused code
    Remove commented-out code for fetching participants

--- test.ts
+++ test.ts
@@ -1,5 +1,3 @@
-// const url = "https://legacy-chat-api-prod.tagmango.com/get-all-participants-by-room?room=66cb77d4422a35f73d21c010&page=289&limit=10"
-
 import path from "node:path";
 
 // const res = await fetch(url);
